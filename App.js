9952d1155faedf00e93fa6e30d0b277478ca7fe5
ManiKumar Gunda <visitmanikumar@gmail.com>
2023-10-22 22:44:04 -0700

    new changes

--- App.js
+++ App.js
@@ -1,6 +1,13 @@
 
 const parent = 
-    React.createElement("div",{id:"child"},[React.createElement("h1",{},"I am h1"),React.createElement("h1",{},"I am h1")]);
+    React.createElement(
+            "div",
+            {   id:"child"  },
+            [
+                React.createElement("h1",{},"I am h1"),
+                React.createElement("h1",{},"I am h1")
+            ]
+        );
     console.log(parent);
 const heading = React.createElement(
     "h1",
